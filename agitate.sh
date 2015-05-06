f44bfeba861cd3f6f1280544b1b4dc5015c6232b
Scott Abernethy <scott@pixietwin.com>
2015-05-07 09:03:31 +1200

    Find and show status as you go

--- agitate.sh
+++ agitate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" == "-q" ]; then
-	QUIETM=1
+	AGITATE_QUIET_MODE=1
 	shift
 fi
 
@@ -10,16 +10,17 @@ if [ -z $1 ]; then
 	SEARCH=$HOME
 fi
 
-if [ -z $QUIETM ]; then
+if [ -z $AGITATE_QUIET_MODE ]; then
 	printf "Agitating all git repositories under %s ...\n" $SEARCH
 fi
-for repo in $(find $SEARCH -type d -name ".git" 2>/dev/null); do
-	cd $(dirname ${repo})
+
+gsummary() {
+	cd $(dirname $1)
 	SHORTDIR=$(pwd | sed 's|.*/\(.*/.*\)|\1|')
 	BRANCHINFO=$(git status -b --porcelain | sed -n 's/^##.*\[\(.*\)\]/[\1]/p' | head -n 1)
 	CHANGED=$(git status --porcelain | sed -n '/^ [ADM]/p' | wc -l | sed 's/ //g')
 	if [ $CHANGED -eq 0 ]; then
-		if [ "$BRANCHINFO" != "" ] || [ -z $QUIETM ]; then
+		if [ "$BRANCHINFO" != "" ] || [ -z $AGITATE_QUIET_MODE ]; then
 			printf "%-30s  \e[1;32m%-12s\e[0m \e[1;36m%s\e[0m\n" $SHORTDIR "clean" "$BRANCHINFO"
 		fi
 	else
@@ -30,5 +31,9 @@ for repo in $(find $SEARCH -type d -name ".git" 2>/dev/null); do
 		fi
 		printf "%-30s  \e[1;31m%-12s\e[0m \e[1;36m%s\e[0m\n" $SHORTDIR "$CHANGED $CHANGED_DESC" "$BRANCHINFO"
 	fi
-	cd - &> /dev/null
-done
+}
+
+export -f gsummary
+export AGITATE_QUIET_MODE
+
+find $SEARCH -type d -name ".git" -exec bash -c 'gsummary "$@"' bash {} \; 2>/dev/null
